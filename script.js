b840b70831c37142735ed767290e1cc40bd956fb
sajalsharma080801 <139940952+sajalsharma080801@users.noreply.github.com>
2023-09-25 19:46:59 +0530

    Add files via upload
    Ok

---
+++ script.js
@@ -0,0 +1,60 @@
+let inputSlider = document.getElementById("inputSlider");
+let sliderValue = document.getElementById("sliderValue");
+let passBox = document.getElementById("passBox");
+let lowercase = document.getElementById("lowercase");
+let uppercase = document.getElementById("uppercase");
+let Numbers = document.getElementById("Numbers");
+let Symbols = document.getElementById("Symbols");
+let genBtn = document.getElementById("genBtn");
+let copyIcon = document.getElementById("copyIcon");
+
+// showing inputslider value
+sliderValue.textContent = inputSlider.value;
+inputSlider.addEventListener('input', () => {
+    sliderValue.textContent = inputSlider.value;
+})
+
+genBtn.addEventListener('click', () => {
+    passBox.value = generatePassword();
+})
+
+let lowerChars = "abcdefghijklmnopqrstuvwxyz";
+let upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let allNumbers = "0123456789";
+let allSymbols = "~!@#$%^&*";
+
+// Function to generate password
+function generatePassword() {
+    let genPassword = "";
+    let allChars = "";
+
+    allChars += lowercase.checked ? lowerChars : "";
+    allChars += uppercase.checked ? upperChars : "";
+    allChars += Numbers.checked ? allNumbers : "";
+    allChars += Symbols.checked ? allSymbols : "";
+
+    if(allChars == "" || allChars.length ==0){
+        return genPassword;
+    }
+
+    let i=1;
+    while (i <= inputSlider.value) {
+        genPassword += allChars.charAt(Math.floor(Math.random() * allChars.length));
+        i++;
+    }
+
+    return genPassword;
+}
+
+copyIcon.addEventListener('click', ()=>{
+    if(passBox.value != "" || passBox.value.length>=1){
+    navigator.clipboard.writeText(passBox.value);
+    copyIcon.innerText = "check";
+    copyIcon.title = "Password Copied";
+
+    setTimeout(()=>{
+        copyIcon.innerHTML = "content_copy";
+        copyIcon.title = "";
+    }, 4000)
+}
+});
